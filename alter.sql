d4300d1e8d14c889b379d04a59c8dd7188e2486a
manik <maniksarker652@gmail.com>
2025-04-21 07:59:38 +0600

    learn simple alter command for update operations

--- alter.sql
+++ alter.sql
@@ -5,3 +5,29 @@
 
 SELECT * from test_table;
 
+
+ALTER TABLE test_table
+ADD COLUMN email VARCHAR(25) DEFAULT 'defaul@gmail.com' NOT NULL;
+
+
+INSERT into test_table VALUES(1,'test',34,'test@gmail.com');
+
+
+ALTER Table test_table 
+ADD PRIMARY KEY (id); 
+
+
+ALTER table test_table 
+RENAME COLUMN age to user_age;
+
+
+ALTER TABLE test_table
+ALTER COLUMN email TYPE VARCHAR(50);
+
+
+ALTER Table test_table
+ALter COLUMN user_age set NOT NULL;
+
+
+ALTER Table test_table
+ALter COLUMN user_age DROP NOT NULL;
