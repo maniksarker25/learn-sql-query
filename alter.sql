e82b0a8f018429d937b7280bb8e41ad24474a93c
manik <maniksarker652@gmail.com>
2025-04-21 08:12:03 +0600

    learn about truncate

--- alter.sql
+++ alter.sql
@@ -6,37 +6,41 @@
 SELECT * from test_table;
 
 
+/* Add new Column----------- */
 ALTER TABLE test_table
 ADD COLUMN email VARCHAR(25) DEFAULT 'defaul@gmail.com' NOT NULL;
 
 
 INSERT into test_table VALUES(1,'test',34,'test@gmail.com');
 
-
+/* Add primary key for a column in table--------- */
 ALTER Table test_table 
 ADD PRIMARY KEY (id); 
 
-
+/* Rename column name --------------- */
 ALTER table test_table 
 RENAME COLUMN age to user_age;
 
 
+/* Change column type ----------- */
 ALTER TABLE test_table
 ALTER COLUMN email TYPE VARCHAR(50);
 
-
+/* Set constrani for a column---------- */
 ALTER Table test_table
 ALter COLUMN user_age set NOT NULL;
 
-
+/* Drop constraint --------------- */
 ALTER Table test_table
 ALter COLUMN user_age DROP NOT NULL;
 
-
+/* Add unique constraint -------------- */
 ALTER Table test_table
 Add Constraint unique_test_table_user_age UNIQUE(user_age);
 
-
+/* Drop constraint --------------- */
 ALTER Table test_table
 DROP constraint unique_test_table_user_age;
 
+/* Truncate : for delete all data from a table but stay table structure ---------- */
+TRUNCATE table test_table;
