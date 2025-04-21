4f923524a4ff26bcacfbc840ce3a34a7ff391621
manik <maniksarker652@gmail.com>
2025-04-21 07:39:56 +0600

    setup local database for sql quires

---
+++ alter.sql
@@ -0,0 +1,7 @@
+-- Active: 1745199090193@@127.0.0.1@5432@test_db
+-- Create table test_table(id INTEGER,name VARCHAR(50),age INTEGER);
+-- INSERT INTO test_table (id ,name,age)
+-- VALUES(1,'manik',20);
+
+SELECT * from test_table;
+
